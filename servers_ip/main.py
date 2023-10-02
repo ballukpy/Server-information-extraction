8862c4a20a5dc7eb0eef225a56d5c53871c3a441
Balluk <navid.arteshi79@gmail.com>
2023-10-02 22:12:44 +0330

    0.7v
    In this update, we create a custom database from the obtained IP information, and we also changed the api key in this update.

--- servers_ip/main.py
+++ servers_ip/main.py
@@ -3,7 +3,8 @@ import subprocess
 import req 
 
 class IPextraction:
-
+    def __init__(self) :
+        self.db={}
 
     def ip(self):
         output = subprocess.check_output(["netstat", "-a", "-n"]).decode("utf-8")
@@ -15,19 +16,32 @@ class IPextraction:
         for ip_address in unique_ips:
             if ip_address == "0.0.0.0" or ip_address == "127.0.0.1":
                 continue
-            if ip_address.startswith("192.168") or ip_address.startswith("192.18.0") or ip_address.startswith("192.0") == True: #filter local ips
+            if ip_address.startswith("192.") or ip_address.startswith("10.") or ip_address.startswith("172.") == True: #filter local ips
                 continue
             filtered_ips.append(ip_address)
 
         return filtered_ips
 
-    def final(self):
+    def database(self):
         ipaddresses = self.ip()
         for ipaddress in ipaddresses:
             info=req.reqip(ipaddress)
-            print(info.viewinformation())
-        
+            main_db=info.getinformation()
+            self.db["p"+str(len(self.db))]={
+            "ip":main_db["ip"],
+            "continent_name":main_db["continent_name"],
+            "country_code":main_db["country_code2"],
+            "country_name":main_db["country_name"],
+            "country_capital":main_db["country_capital"],
+            "city":main_db["city"],
+            "calling_code":main_db["calling_code"],
+            "isp":main_db["isp"],
+            "connection_type":main_db["connection_type"],
+            "organization":main_db["organization"]}
+        return self.db
+
+
 
 ipex=IPextraction()
-ipex.final()
-# print(ipex.ip())
+print(ipex.database())
+
--- servers_ip/req.py
+++ servers_ip/req.py
@@ -2,32 +2,11 @@ import requests
 class reqip:
     def __init__(self,ip):
         self.ip=ip
-   
     def getinformation(self):
         url="https://api.ipgeolocation.io/ipgeo?apiKey="
-        api_key="c3a30ca5d83d4b6c9e761577d754cebb"
+        api_key="edbe5ec010a3457a82ef63b56acad5f2"
         response=requests.get(url + api_key + "&ip=" + self.ip)
         self.dict_infodict_info=response.json()
         return self.dict_infodict_info
+    
 
-    def viewinformation(self):
-        info=self.getinformation()
-        print(f"""ip : {info["ip"]}
-continent name : {info["continent_name"]}
-country code : {info["country_code2"]}
-country name : {info["country_name"]}
-country capital : {info["country_capital"]}
-city : {info["city"]}
-calling code : {info["calling_code"]}
-isp : {info["isp"]}
-connection type : {info["connection_type"]}
-organization : {info["organization"]}""")
-        
-
-def final():
-    ipaddresses=['35.186.224.25', '172.67.17.71', '162.159.134.234', '20.198.119.84', '138.199.12.82', '104.20.62.122']
-    for ipaddress in ipaddresses:
-        info=reqip(ipaddress)
-        print(info.viewinformation())
-
-final()
