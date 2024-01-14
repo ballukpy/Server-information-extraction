5aa9c713867d9bb4420c0f4f426d7942cc1ad550
Balluk <navid.arteshi79@gmail.com>
2024-01-14 18:32:11 +0330

    this feature is available to all paid subscription only

---
+++ src/domain_ip_ext.py
@@ -0,0 +1,14 @@
+import requests
+class reqip:
+    def __init__(self,ip):
+        self.ip=ip
+    def getinformation(self):
+        url="https://api.ipgeolocation.io/ipgeo?apiKey="
+        api_key="edbe5ec010a3457a82ef63b56acad5f2"
+        response=requests.get(url + api_key + "&ip=" + self.ip)
+        self.dict_infodict_info=response.json()
+        return self.dict_infodict_info
+    
+
+navmain=reqip("https://www.refah-bank.ir")
+print(navmain.getinformation())
